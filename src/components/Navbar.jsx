7b1c2a4eed6749016e3fb5fe5726aae6451ec910
Dhruba1010 <dhrubasinha096p@gmail.com>
2022-05-14 12:36:13 +0530

    initial

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Navbar = () => {
+
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    height: 60px;
+    background-color: #131921;
+`
